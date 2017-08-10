d565d5d6a9cfcae0e72f3a4110af54737871f5f7
LyNguyen <cnttnt@gmail.com>
2017-08-10 14:54:18 +0700

    views

--- app/Http/Middleware/AdminLoginMiddleware.php
+++ app/Http/Middleware/AdminLoginMiddleware.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Auth;
 
 class AdminLoginMiddleware
 {
@@ -15,6 +16,14 @@ class AdminLoginMiddleware
      */
     public function handle($request, Closure $next)
     {
-        return $next($request);
+        if(Auth::check()){
+            $user = Auth::user();
+            if($user->is_admin == 1){
+                return $next($request);
+            }
+        }
+        else{
+            return redirect('dashboard/login');
+        }
     }
 }
