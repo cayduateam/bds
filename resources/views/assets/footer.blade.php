5dd97508017f10fd6bf5d73b1406302fd1c48baa
LyNguyen <cnttnt@gmail.com>
2017-08-10 15:58:40 +0700

    index page

--- resources/views/assets/footer.blade.php
+++ resources/views/assets/footer.blade.php
@@ -1,7 +1,7 @@
 <footer id="footer"> 
 	<div class="container"> 
 		<div class="text-center"> 
-			<p>Copyright &copy; 2017 - <a href="http://mostafiz.me/">Team Building</a> Thuy</p> 
+			<p>Copyright &copy; 2017 - <a href="http://laravel.cansolution.vn/">Team Building</a> Thuy</p> 
 		</div> 
 	</div> 
 </footer> <!--/#footer--> 
