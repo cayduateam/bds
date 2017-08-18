6aca082f785cf1ed3873f61f99ca3d4e82694e17
LyNguyen <cnttnt@gmail.com>
2017-08-18 09:46:16 +0700

    180817

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -1,75 +1,101 @@
 @extends('assets.master')
 
 @section('style')
-	<link rel="stylesheet" href="css/owlslider/owl.carousel.min.css">
-	<link rel="stylesheet" href="css/owlslider/owl.theme.default.min.css">
-	<link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/index.css">
 @endsection
 
 @section('script')
-	<script src="js/owl.carousel.min.js"></script>
-	<script src="js/index.js"></script>
+    <script src="assets/js/owl.carousel.js"></script>
+    <script src="js/index.js"></script>
 @endsection
 
 @section('content')
-	{{--slider--}}
-	<div class="owl-carousel">
-		<div class="article parallax" data-speed="0.5" style="background-image: url(images/slider/slide3.jpg)">
-			<div class="content-slider">
-				<h3 class="heading animated bounceInDown">KHU ĐÔ THỊ LÊ HỒNG PHONG II</h3>
-				<p class="animated bounceInUp">Fully Professional one page template</p>
-				<a class="btn btn-default slider-btn animated fadeIn" href="#">Xem ngay</a>
-			</div>
-		</div>
-		<div class="article parallax" data-speed="0.5" style="background-image: url(images/slider/slide2.jpg)">
-			<div class="content-slider">
-				<h3 class="heading animated bounceInDown">KHU ĐÔ THỊ LÊ HỒNG PHONG II</h3>
-				<p class="animated bounceInUp">Fully Professional one page template</p>
-				<a class="btn btn-default slider-btn animated fadeIn" href="#">Xem ngay</a>
-			</div>
-		</div>
-		<div class="article parallax" data-speed="0.5" style="background-image: url(images/slider/slide1.jpg)">
-			<div class="content-slider">
-				<h3 class="heading animated bounceInDown">Khu đô thị Venesia</h3>
-				<p class="animated bounceInUp">Fully Professional one page template</p>
-				<a class="btn btn-default slider-btn animated fadeIn" href="#">Xem ngay</a>
-			</div>
-		</div>
-		<div class="article parallax" data-speed="0.5" style="background-image: url(images/slider/slide2.jpg)">
-			<div class="content-slider">
-				<h3 class="heading animated bounceInDown">Tận hưởng cuộc sống tuyệt vời tại Venice phương Đông</h3>
-				<p class="animated bounceInUp">Fully Professional one page template</p>
-				<a class="btn btn-default slider-btn animated fadeIn" href="#">Xem ngay</a>
-			</div>
-		</div>
-	</div>
-	{{--end slider--}}
+    {{--slider--}}
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img class="d-block w-100" src="images/slider/slide3.jpg" alt="First slide">
+            <div class="content-slider">
+                <h3 class="heading wow animated bounceInDown">KHU ĐÔ THỊ LÊ HỒNG PHONG II</h3>
+                <p class="animated bounceInUp">Fully Professional one page template</p>
+                
+            </div>
+            <div class="link-slider">
+                <a class="btn btn-default slider-btn animated fadeIn" href="#">Xem ngay</a>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="images/slider/slide2.jpg" alt="Second slide">
+            <div class="content-slider">
+                <h3 class="heading wow animated bounceInDown">KHU ĐÔ THỊ LÊ HỒNG PHONG II</h3>
+                <p class="animated bounceInUp">Fully Professional one page template</p>
+            </div>
+            <div class="link-slider">
+                <a class="btn btn-default slider-btn animated fadeIn" href="#">Xem ngay</a>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="images/slider/slide1.jpg" alt="Third slide">
+            <div class="content-slider">
+                <h3 class="heading wow animated bounceInDown">KHU ĐÔ THỊ LÊ HỒNG PHONG II</h3>
+                <p class="animated wow bounceInUp">Fully Professional one page template</p>
+            </div>
+            <div class="link-slider">
+                <a class="btn btn-default slider-btn animated fadeIn" href="#">Xem ngay</a>
+            </div>
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+    {{--end slider--}}
 
-	{{--service--}}
-	<div class="text-center service container">
-		<h2 class="title animated wobble">DỊCH VỤ CỦA CHÚNG TÔI</h2>
-		<div class="row">
-			<div class="col-md-6 service-right">
-				<ul class="list-1 list-left right wow bounceInLeft animated">
-					<li><span class="circle"><i class="fa fa-universal-access" aria-hidden="true"></i></span><span class="autoLeft fixTop">Chuyên cung cấp các dịch vụ kinh doanh, môi giới, thẩm định, quảng cáo, đấu giá bất động sản.</span><div class="clear"></div></li>
-					<li><span class="circle"><i class="fa fa-diamond" aria-hidden="true"></i></span><span class="autoLeft fixTop">Tư vấn pháp lý, tín dụng, thanh toán chuyển nhượng bất động sản.</span><div class="clear"></div></li>
-				</ul>
-			</div>
-			<div class="col-md-6">
-				<ul class="list-1 list-right animated bounceInRight">
-					<li><span class="circle"><i class="fa fa-users" aria-hidden="true"></i></span><span class="autoLeft fixTop">Tư vấn thiết kế, nội thất dân dụng.</strong></span><div class="clear"></div></li>
-				</ul>
-				<ul class="list-1 list-right animated bounceInRight">
-					<li><span class="circle"><i class="fa fa-users" aria-hidden="true"></i></span><span class="autoLeft fixTop">Dịch vụ4 <strong>3333333333</strong></span><div class="clear"></div></li>
-				</ul>
-			</div>
-		</div>
-	</div>
-	<p class="line"></p>
-	{{--end service --}}
+    {{--service--}}
+    <p class="line"></p>
+    <div class="service container-fluid">
+        <h2 class="title animated wobble text-center hidden-xs-down">DỊCH VỤ CỦA CHÚNG TÔI</h2>
+        <div class="row">
+            <div class="col-md-6 col-xs-12 service-left">
+                <ul>
+                    <li>
+                        <span class="text-right circle">
+                            <i class="fa fa-line-chart" aria-hidden="true"></i>
+                        </span>
+                        <span class="text">
+                            Chuyên cung cấp các dịch vụ kinh doanh, môi giới, thẩm định, quảng cáo, đấu giá bất động sản
+                        </span>
+                    </li>
+                    <li>
+                        <span class="text-right circle">
+                            <i class="fa fa-university" aria-hidden="true"></i>
+                        </span>
+                        <span class="text">
+                            Tư vấn pháp lý, tín dụng, thanh toán chuyển nhượng bất động sản.
+                        </span>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-6 col-xs-12 service-right">
+                <i class="fa fa-quote-left" aria-hidden="true"></i>
+                <div class="detail">
+                    <p>Vui lòng liên hệ để được tư vẫn hỗ trợ nhanh chóng nhất</p>
+                    <P>Ms Thủy: 0935 044 567</P>
+                </div>
+                <i class="fa fa-quote-right" aria-hidden="true"></i>
+            </div>
+        </div>
+    </div>
+    <p class="line"></p>
+    {{--end service --}}
 
-	{{--search with parallax --}}
-	<div class="searchbds parallax" data-speed="0.5" style="background: url('images/search-background.jpg') 50% -79.5px no-repeat;">
+    {{--search with parallax --}}
+    <!-- <div class="searchbds parallax" data-speed="0.5" style="background: url('images/search-background.jpg') 50% -79.5px no-repeat;">
 
         <div class="container searchbds-container">
             <div class="row">
@@ -139,8 +165,8 @@
                 </div>
             </div>
         </div>
-	</div>
-	{{--end search with parallax --}}
+    </div> -->
+    {{--end search with parallax --}}
 
     {{--lastest news--}}
 
