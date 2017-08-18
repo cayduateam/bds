7f36bfc3d4cd437e11647d89ad8a784facb15483
LyNguyen <cnttnt@gmail.com>
2017-08-18 10:28:03 +0700

    180817

--- resources/views/assets/footer.blade.php
+++ resources/views/assets/footer.blade.php
@@ -2,15 +2,18 @@
 	<div class="footer1">
 		<div class="container">
 			<div class="row">
-				<div class="col-md-3">
-					<a href="index.html"><img src="images/logo.png" alt="logo" style="max-height:50px"></a>
-					<div class="titleFooter">BĐS Nha Trang Online team</div>
-					<div><div aria-hidden="true" class="fa fa-map-marker iconLocation">&nbsp;</div>48 DTH, Nha Trang, Khánh Hòa</div>
-					<div><div aria-hidden="true" class="fa fa-envelope iconEmail">&nbsp;</div>Email: <a href="mailto:cnttnt@gmail.com">hotroaaaavn</a></div>
-					<div><div aria-hidden="true" class="fa fa-phone iconPhone">&nbsp;</div>Điện thoại: 093 553 9933</div>
-					<div><div aria-hidden="true" class="fa fa-home iconHome">&nbsp;</div>Website: <a href="http://bdsnhatrangonline.vn" target="_blank" title="bdsnhatrangonline">bdsnhatrangonline.vn</a></div>
+				<div class="col-md-6">
+					<div class="logo">
+						<a href="/"><img src="images/logo2.png" alt="logo" style="max-height:50px"></a>
+						<h4 class="titleFooter">BĐS Nha Trang Online team</</h4>
+					</div>
+					<div class="content">
+						<p><span aria-hidden="true" class="fa fa-map-marker iconLocation">&nbsp;</span>48 DTH, Nha Trang, Khánh Hòa</p>
+						<p><span aria-hidden="true" class="fa fa-envelope iconEmail">&nbsp;</span>Email: <a href="mailto:cnttnt@gmail.com">hotroaaaavn</a></p>
+						<p><span aria-hidden="true" class="fa fa-phone iconPhone">&nbsp;</span>Điện thoại: 093 553 9933</p>
+						<p><span aria-hidden="true" class="fa fa-home iconHome">&nbsp;</span>Website: <a href="http://bdsnhatrangonline.vn" target="_blank" title="bdsnhatrangonline">bdsnhatrangonline.vn</a></p>
+					</div>
 				</div>
-				<div class="col-md-3"></div>
 				<div class="col-md-6">
 					<div class="fb-page" data-href="https://www.facebook.com/Si%C3%AAu-Th%E1%BB%8B-D%E1%BB%B1-%C3%81n-%C4%90%E1%BA%A5t-N%E1%BB%81n-Nha-Trang-Kh%C3%A1nh-Ho%C3%A0-771903592976372/" data-width="550" data-height="300" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/Si%C3%AAu-Th%E1%BB%8B-D%E1%BB%B1-%C3%81n-%C4%90%E1%BA%A5t-N%E1%BB%81n-Nha-Trang-Kh%C3%A1nh-Ho%C3%A0-771903592976372/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/Si%C3%AAu-Th%E1%BB%8B-D%E1%BB%B1-%C3%81n-%C4%90%E1%BA%A5t-N%E1%BB%81n-Nha-Trang-Kh%C3%A1nh-Ho%C3%A0-771903592976372/">Siêu Thị Dự Án Đất Nền Nha Trang Khánh Hoà</a></blockquote></div>
 				</div>
