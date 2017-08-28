ba5284dbbf9173a8d889e69540619fba46278304
LyNguyen <cnttnt@gmail.com>
2017-08-28 19:33:44 +0700

    install Intervention Image

--- config/app.php
+++ config/app.php
@@ -178,6 +178,8 @@ return [
         App\Providers\RouteServiceProvider::class,
         //120817 add
         Collective\Html\HtmlServiceProvider::class,
+
+        Intervention\Image\ImageServiceProvider::class
     ],
 
     /*
@@ -230,6 +232,8 @@ return [
         //120817 add
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+
+        'Image' => Intervention\Image\Facades\Image::class
     ],
 
 ];
